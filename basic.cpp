788d01e8c2bdd916640947d7a2f8edda55486c4b
murtaza <93817703+murtazabazaz@users.noreply.github.com>
2023-12-18 20:44:05 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+    {
+        cout<<"hello"<<endl;
+        float bloodSugar =2.2;
+        cout<<bloodSugar<<endl;
+        int x;
+        cout<<"enter age"<<endl;
+        cin>>x;
+        cout<<"Your blood Sugar is "<<bloodSugar<<"mg/dL at the age of "<<x<<" it is dangerous."<<endl;
+        int a, b;
+        int sum;
+        cout<<"enter a number = "<<endl;
+        cin>>a;
+        cout<<"enter another number = "<<endl;
+        cin>>b;
+        sum=(a+b)22/2*100;
+        cout<<sum;
+
+
+    }
